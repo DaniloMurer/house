d2e7a67e9047401dcaccec714fd60c251411aaa9
Danilo Jakob <danilojakob@protonmail.com>
2024-06-26 19:31:37 +0200

    dev: what am i even trying to do

--- app.vue
+++ app.vue
@@ -1,12 +1,7 @@
 <script setup lang="ts">
-function fetchData() : any {
-  console.log($fetch('/api/test').then(data => {console.log(data)}))
-}
 </script>
 <template>
-  <div>
-    <!--<NuxtRouteAnnouncer />-->
-    <!--<NuxtWelcome />-->
-    <UButton @click="fetchData()">Button</UButton>
-  </div>
+  <NuxtLayout class="max-h-screen">
+    <NuxtPage />
+  </NuxtLayout>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const route = useRoute()
+function fetchData() : any {
+  console.log($fetch('/api/test').then(data => {console.log(data)}))
+}
+</script>
+
+<template>
+  <div class="flex-container-center max-h-screen">
+    <UButton @click="fetchData()" style="">Button</UButton>
+  </div>
+</template>
